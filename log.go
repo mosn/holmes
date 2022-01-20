1bf50cdcf9ab99eee09b54f8fdfd56b1ee1120ab
songzhibin97 <718428482@qq.com>
2022-01-20 18:14:03 +0800

    fix:lint

--- log.go
+++ log.go
@@ -24,45 +24,47 @@ func (h *Holmes) debugf(pattern string, args ...interface{}) {
 }
 
 func (h *Holmes) writeString(content string) {
-	if h.opts.logOpts.Enable {
-		state, err := h.opts.Logger.Stat()
+	if _, err := h.opts.Logger.WriteString(content); err != nil {
+		fmt.Println(err) // where to write this log?
+	}
+	if !h.opts.logOpts.Enable {
+		return
+	}
+	state, err := h.opts.Logger.Stat()
+	if err != nil {
+		h.opts.logOpts.Enable = false
+
+		return
+	}
+
+	if state.Size() > h.opts.logOpts.SplitLoggerSize && atomic.CompareAndSwapInt32(&h.changelog, 0, 1) {
+		defer atomic.StoreInt32(&h.changelog, 0)
+
+		var (
+			newLogger *os.File
+			err       error
+			suffix    = time.Now().Format("20060102150405")
+			srcPath   = filepath.Join(filepath.Clean(h.opts.DumpPath), defaultLoggerName)
+			dstPath   = filepath.Join(filepath.Clean(h.opts.DumpPath), defaultLoggerName+"_"+suffix+".back")
+		)
+
+		err = os.Rename(srcPath, dstPath)
+
+		if err != nil {
+			h.opts.logOpts.Enable = false
+
+			return
+		}
+		newLogger, err = os.OpenFile(srcPath, defaultLoggerFlags, defaultLoggerPerm)
+
 		if err != nil {
 			h.opts.logOpts.Enable = false
 
 			return
 		}
 
-		if state.Size() > h.opts.logOpts.SplitLoggerSize && atomic.CompareAndSwapInt32(&h.changelog, 0, 1) {
-			defer atomic.StoreInt32(&h.changelog, 0)
-
-			var (
-				newLogger *os.File
-				err       error
-				suffix    = time.Now().Format("20060102150405")
-				srcPath   = filepath.Clean(filepath.Join(h.opts.DumpPath, defaultLoggerName))
-				dstPath   = filepath.Clean(filepath.Join(h.opts.DumpPath, defaultLoggerName+"_"+suffix+".back"))
-			)
-
-			err = os.Rename(srcPath, dstPath)
-
-			if err != nil {
-				h.opts.logOpts.Enable = false
-
-				return
-			}
-			newLogger, err = os.OpenFile(srcPath, defaultLoggerFlags, defaultLoggerPerm)
-
-			if err != nil {
-				h.opts.logOpts.Enable = false
-
-				return
-			}
-
-			h.opts.Logger, newLogger = newLogger, h.opts.Logger
-			_ = newLogger.Close()
-		}
+		h.opts.Logger, newLogger = newLogger, h.opts.Logger
+		_ = newLogger.Close()
 	}
-	if _, err := h.opts.Logger.WriteString(content); err != nil {
-		fmt.Println(err) // where to write this log?
-	}
+
 }
